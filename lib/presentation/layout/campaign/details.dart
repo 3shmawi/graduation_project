75d77f9e11b1808bf38339e6604049bc19174cca
ashmawi <mohamedashmawy918@gmail.com>
2023-12-05 19:27:31 +0200

    layout ui >> campaign detail page

--- lib/presentation/layout/campaign/details.dart
+++ lib/presentation/layout/campaign/details.dart
@@ -12,12 +12,15 @@ class CampaignDetailsScreen extends StatefulWidget {
 }
 
 class _CampaignDetailsScreenState extends State<CampaignDetailsScreen> {
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Stack(
         children: [
-          Stack(
+          ListView(
+            padding: EdgeInsets.zero,
             children: [
               const CustomCacheImage(
                 imageUrl:
@@ -25,124 +28,83 @@ class _CampaignDetailsScreenState extends State<CampaignDetailsScreen> {
                 radius: AppSize.s35,
                 circularShape: false,
                 width: double.infinity,
-                height: AppSize.s285,
+                height: AppSize.s255,
               ),
               Padding(
-                padding: const EdgeInsets.symmetric(
-                  vertical: AppPadding.p35,
-                  horizontal: AppSize.s20,
-                ),
-                child: Row(
+                padding: const EdgeInsets.symmetric(horizontal: AppPadding.p14),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    IconButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                      icon: Icon(
-                        Icons.arrow_back_ios,
-                        size: AppSize.s28,
-                        color: AppColors.white,
+                    const HeadLine(
+                      label: 'بالتعليم استطيع : ',
+                    ),
+                    const SizedBox(height: AppSize.s8),
+                    Text(
+                      'يوجد فى العالم 222 مليون طفل لم يلتحقوا بمقاعد التعليم بسبب الفقر ',
+                      style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                            fontSize: FontSize.s16,
+                          ),
+                    ),
+                    const SizedBox(height: AppSize.s28),
+                    const Divider(),
+                    Align(
+                      alignment: Alignment.topRight,
+                      child: Column(
+                        children: [
+                          const HeadLine(
+                            label:
+                                'املأ حقيبة بالعلم والأمل: حملة التبرع بالعدة المدرسية',
+                          ),
+                          Container(
+                            height: AppSize.s1_5,
+                            width: 200,
+                            color: AppColors.primary,
+                          ),
+                        ],
                       ),
                     ),
-                    const Spacer(),
-                    IconButton(
-                      onPressed: () {},
-                      icon: Icon(
-                        Icons.menu,
-                        size: AppSize.s28,
-                        color: AppColors.white,
-                      ),
+                    HeadLine(
+                      label: 'نبذة عن الحملة :',
+                      color: AppColors.primary,
                     ),
-                  ],
-                ),
-              )
-            ],
-          ),
-          Expanded(
-            child: Stack(
-              alignment: AlignmentDirectional.topEnd,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: AppPadding.p20,
-                    vertical: AppPadding.p12,
-                  ),
-                  child: ListView(
-                    padding: EdgeInsets.zero,
-                    shrinkWrap: true,
-                    children: [
-                      const HeadLine(
-                        label: 'بالتعليم استطيع : ',
-                      ),
-                      const SizedBox(height: AppSize.s8),
-                      Text(
-                        'يوجد فى العالم 222 مليون طفل لم يلتحقوا بمقاعد التعليم بسبب الفقر ',
-                        style:
-                            Theme.of(context).textTheme.labelMedium!.copyWith(
-                                  fontSize: FontSize.s16,
-                                ),
-                      ),
-                      const SizedBox(height: AppSize.s28),
-                      const Divider(),
-                      Align(
-                        alignment: Alignment.topRight,
-                        child: Column(
-                          children: [
-                            const HeadLine(
-                              label:
-                                  'املأ حقيبة بالعلم والأمل: حملة التبرع بالعدة المدرسية',
-                            ),
-                            Container(
-                              height: AppSize.s1_5,
-                              width: 200,
-                              color: AppColors.primary,
-                            ),
-                          ],
-                        ),
-                      ),
-                      HeadLine(
-                        label: 'نبذة عن الحملة :',
-                        color: AppColors.primary,
-                      ),
-                      Text(
-                        'ندعوكم للمشاركة فى حملتنا الخيرية “ املا حقيبة بالعلم الامل “التى تهدف الى جمع العدة المدرسية للاطفال الذين يواجهون صعوبات فى الوصول الى المستلزمات المدرسية الاساسية لهذا العام سنقوم بتزيع حقائب مدرسية مجهزة لكل ما يحتاجة الاطفال لبداية دراسية ناجحة ',
-                        style:
-                            Theme.of(context).textTheme.labelMedium!.copyWith(
-                                  fontSize: FontSize.s16,
-                                ),
-                      ),
-                      const SizedBox(height: AppSize.s35),
-                      const Divider(),
-                      const SizedBox(height: AppSize.s16),
-                      HeadLine(
-                        label: 'كيف يمكنك المساهمة: ',
-                        color: AppColors.primary,
-                      ),
-                      Text(
-                        '1. التبرع المالي: يمكنك المساهمة بأي مبلغ تشاء، حيث سيتم استخدام تبرعك لشراء الجهاز وتطويره.',
-                        style:
-                            Theme.of(context).textTheme.labelMedium!.copyWith(
-                                  fontSize: FontSize.s16,
-                                ),
-                      ),
-                      const SizedBox(
-                        height: AppSize.s16,
-                      ),
-                      Text(
-                        '2. نشر الحملة: ساعدنا في نشر هذه الحملة بين أصدقائك وعائلتك وشبكتك الاجتماعية. شاركها على وسائل التواصل وساهم في بناء جسر من الأمل.',
-                        style:
-                            Theme.of(context).textTheme.labelMedium!.copyWith(
-                                  fontSize: FontSize.s16,
-                                ),
-                      ),
-                      const SizedBox(
-                        height: AppSize.s20,
-                      ),
-                      const Divider(),
-                      const HeadLine(label: 'الدول المشاركة فى الحملة :'),
-                      const Wrap(
+                    Text(
+                      'ندعوكم للمشاركة فى حملتنا الخيرية “ املا حقيبة بالعلم الامل “التى تهدف الى جمع العدة المدرسية للاطفال الذين يواجهون صعوبات فى الوصول الى المستلزمات المدرسية الاساسية لهذا العام سنقوم بتزيع حقائب مدرسية مجهزة لكل ما يحتاجة الاطفال لبداية دراسية ناجحة ',
+                      style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                            fontSize: FontSize.s16,
+                          ),
+                    ),
+                    const SizedBox(height: AppSize.s35),
+                    const Divider(),
+                    const SizedBox(height: AppSize.s16),
+                    HeadLine(
+                      label: 'كيف يمكنك المساهمة: ',
+                      color: AppColors.primary,
+                    ),
+                    Text(
+                      '1. التبرع المالي: يمكنك المساهمة بأي مبلغ تشاء، حيث سيتم استخدام تبرعك لشراء الجهاز وتطويره.',
+                      style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                            fontSize: FontSize.s16,
+                          ),
+                    ),
+                    const SizedBox(
+                      height: AppSize.s16,
+                    ),
+                    Text(
+                      '2. نشر الحملة: ساعدنا في نشر هذه الحملة بين أصدقائك وعائلتك وشبكتك الاجتماعية. شاركها على وسائل التواصل وساهم في بناء جسر من الأمل.',
+                      style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                            fontSize: FontSize.s16,
+                          ),
+                    ),
+                    const SizedBox(
+                      height: AppSize.s20,
+                    ),
+                    const Divider(),
+                    const HeadLine(label: 'الدول المشاركة فى الحملة :'),
+                    const Align(
+                      alignment: Alignment.center,
+                      child: Wrap(
                         crossAxisAlignment: WrapCrossAlignment.center,
-                        alignment: WrapAlignment.spaceEvenly,
+                        alignment: WrapAlignment.spaceAround,
                         children: [
                           CountryItem(
                             name: 'السعودية',
@@ -166,67 +128,67 @@ class _CampaignDetailsScreenState extends State<CampaignDetailsScreen> {
                           ),
                         ],
                       ),
-                      const Divider(),
-                      const SizedBox(
-                        height: AppSize.s92,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            ItemBanner(
-                              label: 'الايام المتبقية',
-                              subTitle: '245 يوم',
-                              icon: AppAssets.calender,
-                            ),
-                            VertLine(),
-                            ItemBanner(
-                              label: 'المستفيدن',
-                              subTitle: '100 شخص',
-                              icon: AppAssets.gender,
-                            ),
-                            VertLine(),
-                            ItemBanner(
-                              label: 'المبلغ المطلوب',
-                              subTitle: '1,000,000 E.g',
-                              icon: AppAssets.money,
-                            ),
-                          ],
-                        ),
+                    ),
+                    const Divider(),
+                    const SizedBox(
+                      height: AppSize.s92,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          ItemBanner(
+                            label: 'الايام المتبقية',
+                            subTitle: '245 يوم',
+                            icon: AppAssets.calender,
+                          ),
+                          VertLine(),
+                          ItemBanner(
+                            label: 'المستفيدن',
+                            subTitle: '100 شخص',
+                            icon: AppAssets.gender,
+                          ),
+                          VertLine(),
+                          ItemBanner(
+                            label: 'المبلغ المطلوب',
+                            subTitle: '1,000,000 E.g',
+                            icon: AppAssets.money,
+                          ),
+                        ],
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: AppSize.s28,
-                          horizontal: AppSize.s12,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Expanded(
-                              flex: 3,
-                              child: CustomButton(
-                                label: 'منتجات الحملة',
-                                onPressed: () {},
-                              ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: AppSize.s28,
+                        horizontal: AppSize.s12,
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            flex: 3,
+                            child: CustomButton(
+                              label: 'منتجات الحملة',
+                              onPressed: () {},
                             ),
-                            const SizedBox(width: AppSize.s20),
-                            Expanded(
-                              flex: 4,
-                              child: CustomButton(
-                                label: 'التبرع السريع',
-                                onPressed: () {},
-                                icon: AppAssets.fast,
-                                color: AppColors.white,
-                              ),
+                          ),
+                          const SizedBox(width: AppSize.s20),
+                          Expanded(
+                            flex: 4,
+                            child: CustomButton(
+                              label: 'التبرع السريع',
+                              onPressed: () {},
+                              icon: AppAssets.fast,
+                              color: AppColors.white,
                             ),
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
-                const ShareButton(),
-              ],
-            ),
+              ),
+            ],
           ),
+          const DefaultAppBar()
         ],
       ),
     );
@@ -256,6 +218,30 @@ class HeadLine extends StatelessWidget {
               color: color ?? AppColors.black,
             ),
       ),
+    );
+  }
+}
+
+class DefaultAppBar extends StatelessWidget {
+  const DefaultAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+            color: Colors.blue,
+            size: AppSize.s28,
+          ),
+        ),
+        const Spacer(),
+        const ShareButton(),
+      ],
     );
   }
 }
